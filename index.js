1692b5077209d27038e5f606603b80024b1a05a8
Elton Memishaj <eltoni.memishi@live.com>
2019-04-04 12:31:35 +0200

    + Added "IP" constant to index.js. - Changed mongodb connection string to real db. - Removed Procfile as Heroku will not be used.

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ let cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-mongoose.connect('mongodb://localhost/cinemapwa-api', { useNewUrlParser: true });
+mongoose.connect('mongodb://console.starter-us-west-2.openshift.com/console/project/cinemapwa-api/browse/routes/cinemapwa-db', { useNewUrlParser: true });
 const db = mongoose.connection;
+const ip = process.env.IP || '0.0.0.0';
 const port = process.env.PORT || 8080;
 app.use(cors());
 app.use('/api', apiRoutes);
-app.listen(port, () => console.log('Running on port ' + port));
+app.listen(port, ip, () => console.log(`Running on ${ip}:${port}`));
