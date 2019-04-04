7ffde76cbfe7c04c995283de27f0b0534c351a64
Elton Memishaj <eltoni.memishi@live.com>
2019-04-04 12:59:28 +0200

    - Updated mongodb connection string.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-mongoose.connect('mongodb://console.starter-us-west-2.openshift.com/console/project/cinemapwa-api/browse/routes/cinemapwa-db', { useNewUrlParser: true });
+mongoose.connect('mongodb://cinemapwa-db-cinemapwa-api.7e14.starter-us-west-2.openshiftapps.com/cinemapwa-db', { useNewUrlParser: true });
 const db = mongoose.connection;
 const ip = process.env.IP || '0.0.0.0';
 const port = process.env.PORT || 8080;
